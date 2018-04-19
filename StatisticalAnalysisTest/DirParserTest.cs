454df4e9878e7ac2a6f99621f237c00dbacc09d0
Kirill Boika <boika.kv@yandex.by>
2018-04-19 19:43:27 +0300

    Объединил Юнит тесты в один проект StatisticalAnalysisTest

---
+++ StatisticalAnalysisTest/DirParserTest.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using StatisticalAnalysis;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DirParserTest
+{
+    [TestClass]
+    public class DirParserTest
+    {
+        private DirParser dirParser;
+
+        [TestMethod]
+        public void DirParser_Happy_Path()
+        {
+            var expectedFileNames = new List<string>()
+            {
+                "test-file.txt",
+                "test-sub-dir-file.txt"
+            };
+
+            dirParser = new DirParser("test-dirParser");
+            var files = dirParser.FileList;
+            var actualFileNames = new List<string>();
+            foreach (var file in files)
+            {
+                actualFileNames.Add(Path.GetFileName(file));
+            }
+
+            Assert.IsTrue(expectedFileNames.SequenceEqual(actualFileNames));
+        }
+    }
+}
---
+++ TempDirectoryTest/1.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using StatisticalAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TempDirectoryTest
+{
+    [TestClass]
+    public class TempDirectoryTest
+    {
+        private string dirName;
+
+        [TestMethod]
+        public void TempDirectory_Directory_Is_Created()
+        {
+            var tempDir = new TempDirectory();
+            dirName = tempDir.Name;
+            Assert.IsTrue(Directory.Exists(dirName));
+        }
+
+        [TestMethod]
+        public void TempDirectory_Directory_Is_Deleted()
+        {
+            Assert.IsFalse(Directory.Exists(dirName));
+        }
+    }
+}
